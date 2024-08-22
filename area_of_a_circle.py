b40628def501937c7dcfb437c2c7dcb3e37ec2c6
Ifeoluwa <os.ifeoluwa@gmail.com>
2024-08-22 17:22:28 +0100

    Updated files

---
+++ area_of_a_circle.py
@@ -0,0 +1,15 @@
+'''
+create a variable name radius
+store the value 10
+create a variable named pie
+store the value 3.142 in variable pie
+calculate aea of the circle using the formular
+store the result in a variable name area
+display the result
+'''
+
+
+radius = int(input("Enter radius: "))
+PI = 3.142
+area = PI * (radius ** 2)
+print("The area of the circle with ", radius, "is ", area)
